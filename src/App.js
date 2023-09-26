1714988f49ed29836536a1126f7e7e837889ac3d
LeeSeongWon05 <lsw050103@gmail.com>
2023-09-26 14:44:08 +0900

    네번째 저장

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './styles/Global.scss';
-
+import { Route } from 'react-router-dom';
 import Department from './components/sub/department/Department';
 import Header from './components/common/header/Header';
+import Youtube from './components/sub/youtube/Youtube';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Department />
+			<Route path='/department'>
+				<Department />
+			</Route>
+
+			<Route path='/Youtube'>
+				<Youtube />
+			</Route>
 		</>
 	);
 }
---
+++ src/components/sub/youtube/Youtube.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../layout/Layout';
+//import styles from './Department.module.scss';
+//import clsx from 'clsx';
+
+export default function Youtube() {
+	return (
+		<Layout title={'Youtube'}>
+			<p>Youtube Page 입니다.</p>
+		</Layout>
+	);
+}
